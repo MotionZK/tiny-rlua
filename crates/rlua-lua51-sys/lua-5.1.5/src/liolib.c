07278fa8a7f72b62d68c69f8f1f7e1dbfadab422
odyzzey <14829937+odyzzey@users.noreply.github.com>
2023-06-23 22:19:15 -0500

    Silence linking error

--- crates/rlua-lua51-sys/lua-5.1.5/src/liolib.c
+++ crates/rlua-lua51-sys/lua-5.1.5/src/liolib.c
@@ -456,7 +456,7 @@ static int f_seek (lua_State *L) {
 
 
 static int f_setvbuf (lua_State *L) {
-  static const int mode[] = {_IONBF, _IOFBF, _IOLBF};
+  static const int mode[] = {_IONBF};
   static const char *const modenames[] = {"no", "full", "line", NULL};
   FILE *f = tofile(L);
   int op = luaL_checkoption(L, 2, NULL, modenames);
